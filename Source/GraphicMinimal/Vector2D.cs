28423408032e6c4391dd24df521f7deb3cf1154b
Enrico Pohl <enricopohl87@googlemail.com>
2023-06-09 06:39:58 +0200

    Add Abs-Function to Vector2D

--- Source/GraphicMinimal/Vector2D.cs
+++ Source/GraphicMinimal/Vector2D.cs
@@ -92,6 +92,11 @@ namespace GraphicMinimal
             return this / Length();
         }
 
+        public Vector2D Abs()
+        {
+            return new Vector2D(Math.Abs(this.X), Math.Abs(this.Y));
+        }
+
         public static Vector2D operator +(Vector2D v1, Vector2D v2)
         {
             return new Vector2D(v1.X + v2.X, v1.Y + v2.Y);
